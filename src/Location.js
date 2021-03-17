c4e2bf80ea657a0772507e81c76d6b202960de1e
Thien Tuan <chuchuotmayman@gmail.com>
2021-03-17 14:56:34 +0700

    test test

--- src/Location.js
+++ src/Location.js
@@ -34,7 +34,9 @@ export default class Location
 		let nloop = this.link.length;
 		for (let i=0; i<nloop; i++)
 		{
-			this.link[i].btn.dispose();
+			this.link[i].pointer.dispose();
+			this.link[i].preview.dispose();
+			this.link[i].button.dispose();
 			this.link[i].mesh.dispose();
 		}
 	}
@@ -45,13 +47,47 @@ export default class Location
 		box.position = new BABYLON.Vector3(_position[0], _position[1], _position[2]);
 		box.isVisible = false;
 
-		let rect = new BABYLON.GUI.Rectangle();
-		rect.width = "40px";
-		rect.height = "40px";
-		rect.cornerRadius = 20;
+		let rectpre = new BABYLON.GUI.Rectangle();
+        rectpre.width = "300px";
+        rectpre.height = "200px";
+        rectpre.cornerRadius = 40;
+        rectpre.color = "Orange";
+        rectpre.thickness = 2;
+        rectpre.background = "green";
+        engine.advancedTexture.addControl(rectpre);
+        rectpre.linkWithMesh(box);
+        rectpre.linkOffsetY = -150;
+        rectpre.isVisible = false;
+
+		// try
+		let imgPreview = new BABYLON.GUI.Image(_name+"_imgPreview","./asset/skysphere/"+_name+"_pre"+".jpg");
+        rectpre.addControl(imgPreview);
+
+		let line = new BABYLON.GUI.Line();
+        line.lineWidth = 3;
+        line.color = "Orange";
+        line.y2 = 98;
+        // line.dash = [5, 10];
+        line.linkOffsetY = -15;
+        engine.advancedTexture.addControl(line);
+        line.linkWithMesh(box);
+        line.connectedControl = rectpre;
+        line.isVisible = false;
+
+		// Icon walk
+		let rect = new BABYLON.GUI.Ellipse();
+		rect.width = "30px";
+		rect.height = "30px";
+		// rect.cornerRadius = 20;
 		rect.color = "Orange";
-		rect.thickness = 4;
-		rect.background = "green";
+		rect.thickness = 2;
+		rect.background = "transparent"; // "white";//
+        rect.hoverCursor = "pointer";
+        rect.isPointerBlocker = true;
+		rect.drawOutline = true;
+		// rect.outlineColor = "Black";
+		// rect.outlineWidth = 5;
+
 		engine.advancedTexture.addControl(rect);
 
 		rect.linkWithMesh(box);
@@ -61,27 +97,27 @@ export default class Location
 			engine.loc = DataSource.getLocationInfo(_name);
 		});
 
+		rect.onPointerMoveObservable.add(() => {
+            rectpre.isVisible = true;
+            line.isVisible = true;
+        });
+
+        rect.onPointerOutObservable.add(() => {
+            rectpre.isVisible = false;
+            line.isVisible = false;
+        });
+
+		// Walk icon
+		let imgwalk = new BABYLON.GUI.Image(+"walkIcon","./asset/icon/walk.png");
+        rect.addControl(imgwalk);
+
+		
 		this.link.push({
 			name: _name,
 			mesh: box,
-			btn: rect
+			button: rect,
+			pointer: line,
+			preview: rectpre
 		});
-		
-		// let imgLogo1 = new BABYLON.GUI.Image("logo", "./asset/menu/giphy.gif");
-		// imgLogo1.top=0;
-		// imgLogo1.isVisible = true;
-
-		// var rect1 = new BABYLON.GUI.Rectangle();
-	    // rect1.width = 0.2;
-	    // rect1.height = 0.2;
-	    // rect1.cornerRadius = 20;
-	    // rect1.color = "green";
-	    // rect1.thickness = 4;
-	 
-	    // rect1.addControl(imgLogo1);
-	    // rect1.onPointerMoveObservable.add(() =>{
-		// 	console.log('imgLogo1');
-	    // });
-	    // engine.advancedTexture.addControl(rect1);
 	}
 }
